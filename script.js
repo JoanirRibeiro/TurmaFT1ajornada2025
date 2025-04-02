70ea812a48d379f013c798c02ff05ae138ba7e26
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-04-02 19:37:00 +0000

    ,

--- script.js
+++ script.js
@@ -14,4 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.2;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
 })
